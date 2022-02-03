8fec405cb5e9af995cb695caee80674fa5e2b1d3
Bart <bartlomiej.janik@hotmail.fr>
2022-02-03 17:40:25 +0100

    Update checkBalance

--- functions/checkBalance/__init__.py
+++ functions/checkBalance/__init__.py
@@ -7,9 +7,10 @@ import azure.functions as func
 REDIS_SSL_PORT = 6380
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    user_id = req.body.get('user_id')
+    body = req.get_json()
+    user_id = body.get('user_id')
     if not user_id:
-        return func.HttpResponse("Missing user_id", status=404)
+        return func.HttpResponse("Missing user_id", status_code=404)
 
     redis_client = redis.StrictRedis(host=os.environ['REDIS_HOST'],
                                      port=REDIS_SSL_PORT,
@@ -19,4 +20,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if not user_balance:
         user_balance = 0
         redis_client.set(user_id, user_balance)
-    return func.HttpResponse(json.dumps({'balance': user_balance}, status=200))
+    return func.HttpResponse(json.dumps({'balance': user_balance}, status_code=200))
