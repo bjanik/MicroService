18e625270bd08ae685598a247c600f11b9b27740
Bart <bartlomiej.janik@hotmail.fr>
2022-02-04 23:42:43 +0100

    Refacto function

--- functions/consumeQueueMessage/__init__.py
+++ functions/consumeQueueMessage/__init__.py
@@ -1,34 +1,38 @@
 import azure.functions as func
+import json
+import logging
 import os
 import redis
 
 from azure.servicebus import ServiceBusClient
 
-ACTIONS = {
-    'payment': lambda balance, amount: balance + amount,
-    'spending': lambda balance, amount: balance - amount,
-}
-
 REDIS_PORT = 6380
 
-def main(msg: func.ServiceBusMessage):
-    msg_body = msg.get_json().decode('utf-8')
-    user_id = msg_body.get('user_id')
-    amount = msg_body.get('amount')
-    action = msg_body.get('action')
-
-    if not user_id or not amount or not action:
-        return func.HttpResponse("Missing parameters in message", status_code=404)
-    if action not in ACTIONS:
-        return func.HttpResponse("Invalid action", status_code=404) 
-    
-    redisClient = redis.StrictRedis(host=os.environ["REDIS_HOST"],
-                                    port=REDIS_PORT,
-                                    password=os.environ["REDIS_PASSWORD"])
+def get_user_balance(user_id, redisClient):
     user_balance = redisClient.get(user_id)
     if not user_balance:
         user_balance = 0
-    user_balance = ACTIONS[action](user_balance, amount)
-    redisClient.set(user_id, user_balance)
-    return func.HttpResponse(f"New user balance is {user_balance}")
+    user_balance = int(user_balance)
 
+def main(msg: func.ServiceBusMessage):
+    body = json.loads(msg.get_body().decode('utf8'))
+    try:
+        user_id = body['user_id']
+        amount = body['amount']
+        action = body['action']
+    except KeyError as e:
+        return "Missing parameter"
+    if amount < 0:
+        return "Amount must be non-negative"
+    if action not in ["spending", "payment"]:
+        return "Invalid action"
+    redisClient = redis.StrictRedis(host=os.environ["REDIS_HOST"],
+                                    port=REDIS_PORT,
+                                    password=os.environ["REDIS_PASSWORD"],
+                                    decode_responses=True,
+                                    ssl=True)
+    user_balance = get_user_balance(user_id, redisClient)
+    new_user_balance = user_balance + amount if action == "payment" else user_balance - amount
+    redisClient.set(user_id, new_user_balance)
+    logging.info(f"user_balance = {new_user_balance}")
+    return "New user balance is {new_user_balance}"
